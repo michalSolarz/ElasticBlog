f4bea26c265ac39c319ecc7d79d014c7c17099b0
Michał Solarz <michalsolarz83@gmail.com>
2016-10-14 15:12:43 +0200

    Added: - CRUD for posts

--- src/AppBundle/Entity/Post.php
+++ src/AppBundle/Entity/Post.php
@@ -21,5 +21,78 @@ class Post
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
-     */private $id;
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contentStrip;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        $this->setContentStrip($content);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContentStrip()
+    {
+        return $this->contentStrip;
+    }
+
+    /**
+     * @param mixed $contentStrip
+     */
+    public function setContentStrip($contentStrip)
+    {
+        $this->contentStrip = strip_tags($contentStrip);
+    }
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michal
+ * Date: 07.10.16
+ * Time: 09:46
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
